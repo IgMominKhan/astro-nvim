8b497b8d37e3ea410781adf924b9937e2388f480
IgMominKhan <mominkhan8991@gmail.com>
2024-02-04 21:15:37 +0300

    plugin

--- mappings.lua
+++ mappings.lua
@@ -2,4 +2,10 @@ return {
   i = {
     ["<C-s>"] = { "<cmd>w!<cr>", desc = "Save File" }, -- change description but the same command
   },
+  n = {
+    ["<leader>ia"] = { "<cmd>TSToolsAddMissingImports<cr>", desc = "Add missing imports" },
+    ["<leader>ir"] = { "<cmd>TSToolsOrganizeImports<cr>", desc = "sort and remove imports" },
+    ["<leader>iv"] = { "<cmd>TSToolsRemoveUnused<cr>", desc = "remove unused vars" },
+    ["<leader>if"] = { "<cmd>TSToolsFixAll<cr>", desc = "Fix all" },
+  },
 }
