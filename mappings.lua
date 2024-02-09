28abfdd3dfc3ac093ba32cb4c24b798ee914c9ee
Ig Momin Khan <mominkhan8991@gmail.com>
2024-02-09 17:55:38 +0300

    codeium and chatgpt added

--- mappings.lua
+++ mappings.lua
@@ -7,5 +7,13 @@ return {
     ["<leader>ir"] = { "<cmd>TSToolsOrganizeImports<cr>", desc = "sort and remove imports" },
     ["<leader>iv"] = { "<cmd>TSToolsRemoveUnused<cr>", desc = "remove unused vars" },
     ["<leader>if"] = { "<cmd>TSToolsFixAll<cr>", desc = "Fix all" },
+    ["<leader>iR"] = { "<cmd>TSToolsRenameFile<cr>", desc = "Rename file" },
+    ["<leader>x"] = { function() require('nvim-emmet').wrap_with_abbreviation() end, desc = "wrap with abbreviation" }
   },
+  v = {
+    ["<leader>x"] = { function() require('nvim-emmet').wrap_with_abbreviation() end, desc = "wrap with abbreviation" }
+  },
+  t = {
+    ["<ESC>"] = { "<C-\\><C-N>", desc= "Toggle Terminal"}
+  }
 }
---
+++ plugins/nvim-emmet.lua
@@ -0,0 +1,7 @@
+ return {
+   "olrtg/nvim-emmet",
+   event = "BufEnter",
+   -- config = function()
+   --   require('nvim-emmet')
+   -- end
+ }
